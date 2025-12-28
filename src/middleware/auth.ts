ad70703dc5d8d76b98e4ef9c29ab5fa4bb3db91f
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-12-28 13:33:29 +0600

    added middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,9 +1,49 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { NextFunction, Request, Response } from "express";
+import config from "../config";
+import { pool } from "../config/db";
 
-const auth = () => {
-    return async (req: Request, res: Response, next: NextFunction) => {
-        console.log();
+const auth = (...roles: string[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization?.split(" ")[1];
+      console.log(token);
+
+      if (!token) {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+
+      const decoded = jwt.verify(
+        token,
+        config.jwtSecret as string
+      ) as JwtPayload;
+
+      const user = await pool.query(
+        `
+      SELECT id, email, role FROM users WHERE email=$1 
+      `,
+        [decoded.email]
+      );
+      if (user.rows.length === 0) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      req.user = decoded;
+
+      if (roles.length && !roles.includes(decoded.role)) {
+        return res.status(403).json({
+          message: "Forbidden access",
+        });
+      }
+
+      next();
+    } catch (err: any) {
+      res.status(401).json({
+        success: false,
+        message: err.message,
+      });
     }
+  };
 };
 
 export default auth;
--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { userController } from "./user.controller";
+import auth from "../../middleware/auth";
 
 const router = Router();
 
-router.get("/", userController.getAllUsers);
+router.get("/", auth("admin"), userController.getAllUsers);
 
 export const userRoute = router;
