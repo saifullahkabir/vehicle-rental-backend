96f453f414054be3686e9e0d1bfbec177af9aba9
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-12-29 14:20:05 +0600

    updateVehicle query bug fixed

--- src/modules/vehicle/vehicle.service.ts
+++ src/modules/vehicle/vehicle.service.ts
@@ -56,7 +56,7 @@ const updateVehicle = async (id: string, payload: Record<string, any>) => {
 
   const result = await pool.query(
     `
-    UPDATE vehicles SET vehicle_name=$1, type=$2, registration_number=$3, daily_rent_price= $4, availability_status=$5 WHERE id=$6 RETURNING *
+    UPDATE vehicles SET vehicle_name = COALESCE($1, vehicle_name), type = COALESCE($2, type),registration_number = COALESCE($3, registration_number), daily_rent_price = COALESCE($4, daily_rent_price), availability_status = COALESCE($5, availability_status) WHERE id=$6 RETURNING *
     `,
     [
       vehicle_name,
